03d88dd588d564361efc58bbdd676a07542061db
Florian Nadler <florian.nadler1@gmail.com>
2022-06-14 18:01:57 +0200

    Added commentary for ScanView

--- SchauerMarkenerkennung/MVVM/View/ScanView.xaml.cs
+++ SchauerMarkenerkennung/MVVM/View/ScanView.xaml.cs
@@ -17,14 +17,12 @@ using System.Windows.Shapes;
 
 namespace SchauerMarkenerkennung.MVVM.View
 {
-    /// <summary>
-    /// Interaction logic for ScanView.xaml
-    /// </summary>
     public partial class ScanView : UserControl
     {
 
         MarkenContext _db = new MarkenContext();
 
+        //Dient zur Überprüfung ob man das Textfeld zur Eingabe der Markennummern aktiviert hatt.
         bool isStarted = false;
         public ScanView()
         {
@@ -32,6 +30,8 @@ namespace SchauerMarkenerkennung.MVVM.View
             FillLbWithCustomers();
         }
 
+        //Füllt Combobox der Customer mit Kunden Dtos. Diese werden gebraucht, um Id zwischenzuspeicher
+        //um später Ohrmarken auf den selectieren Kunden zuweisen zu können.
         private void FillLbWithCustomers()
         {
             foreach (var kunde in _db.Kunden.ToList())
@@ -44,6 +44,7 @@ namespace SchauerMarkenerkennung.MVVM.View
             }
         }
 
+        //Aktiviert die Eingabe für Markennummern. Benötigte Objekte werden aktiviert/deaktiviert
         private void ButtonStart_Click(object sender, RoutedEventArgs e)
         {
             isStarted = true;
@@ -52,14 +53,17 @@ namespace SchauerMarkenerkennung.MVVM.View
             btnStart.IsEnabled = false;
         }
 
+        //Wird aufgerufen wenn man Markennummer in Textfeld gibt.
         private async void tbInput_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (isStarted && tbInput.Text.Length > 8)
             {
+                //Ruft Task auf, der diese dann nach kurzer Wartezeit in die Listbox überträgt und Textfäld cleared.
                 await AddToListBox();
             }
         }
 
+        //Task der in der obrigen Methode aufgerufen wird. Dient zur Übertragen der Markennummern in Listbox
         private async Task<string> AddToListBox()
         {
             await Task.Delay(1000);
@@ -69,12 +73,15 @@ namespace SchauerMarkenerkennung.MVVM.View
             return tbInput.Text;
         }
 
+        //PreviewTextInput des Textfeldes im XAML ruft diese Methode auf. Dient dazu, dass ausschließlich
+        //Zahlen und keine Buchstaben in Textfeld eingegeben werden können.
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
         }
 
+        //Dient zum Stoppen der Eingabe für Marken. Aktiviert/Deaktiviert benötigte Felder/Buttons.
         private void ButtonStop_Click(object sender, RoutedEventArgs e)
         {
             isStarted = false;
@@ -84,32 +91,37 @@ namespace SchauerMarkenerkennung.MVVM.View
             btnStart.IsEnabled = true;
         }
 
+        //Löscht ausgewählte Markennummer aus Listbox
         private void ButtonDel_Click(object sender, RoutedEventArgs e)
         {
             lbNumbers.Items.Remove(lbNumbers.SelectedItem);
         }
 
+        //Speichert alle Ohrmarken in der Listbox in die Datenbank
         private void ButtonSave_Click(object sender, RoutedEventArgs e)
         {
+            //Funktion CheckTextBoxes prüft ob alle benötigen Felder einen Inhalt haben
             if (CeckTextBoxes())
-            {
+            {   
+                //holt ausgewähltes Kunden Dto aus der Combobox für Kunden
                 KundeDto kunde = lbCustomers.SelectedItem as KundeDto;
+
+                //Kunden ID, prüft ob diese null ist
                 string kundId = "";
                 if (kunde != null)
                 {
                     kundId = kunde.AD_ADRESS_ID;
                 }
-                string cusId = _db.Kunden.Where(x => x.AD_ADRESS_ID == kundId)
-                    .Select(x => x.AD_ADRESS_ID)
-                    .FirstOrDefault();
 
+                //Geht alle Nummern in der Listbox durch und Speichert diese mitsamt den benötigten
+                //Daten in die Datenbank
                 foreach (string oNumber in lbNumbers.Items)
                 {
                     var ohrmarke = new Ohrmarke
                     {
                         Beschreibung = tbBeschreibung.Text,
                         Datum = DateTime.Now,
-                        KundeAD_ADRESS_ID = cusId,
+                        KundeAD_ADRESS_ID = kundId,
                         MarkenNummer = oNumber,
                         Kommissionierer = tbKommisionier.Text,
                         Markentyp = tbType.Text
@@ -117,10 +129,13 @@ namespace SchauerMarkenerkennung.MVVM.View
                     _db.Ohrmarken.Add(ohrmarke);
                 }
                 _db.SaveChanges();
+
+                //Methode wird nach erfolgreichem Speichern aufgerufen um Textfelder und Listbox zu leeren
                 ClearTextBoxes();
             }
         }
 
+        //Leert Textfelder und Listbox, versteck die Customerinformation wieder
         private void ClearTextBoxes()
         {
             tbBeschreibung.Text = "";
@@ -131,6 +146,9 @@ namespace SchauerMarkenerkennung.MVVM.View
             dgCustomerInfo.Visibility = Visibility.Hidden;
         }
 
+        //Überprüft ob Textfelder leer sind. Ist dass der Fall, wird ein Hinweis eingeblendet,
+        //dass man etwas in dieses Feld eintragen muss. Wenn Feld bereits ausgefüllt ist, wird
+        //dieser Hinweis versteckt
         private bool CeckTextBoxes()
         {
             bool allFieldsFilled = true;
@@ -188,11 +206,13 @@ namespace SchauerMarkenerkennung.MVVM.View
             return allFieldsFilled;
         }
 
+        //Reagiert auf neues Auswählen eines Kunden in der Datenbank und zeig anschließend mehr Information dazu an
         private void lbCustomers_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var selKunde = lbCustomers.SelectedItem as KundeDto;
 
-
+            //Information wird in ein Datagrid eingetragen. Dazu wird eine eigene Klasse GridKunde verwendet. Diese
+            //beinhaltet lediglich die benötigten Informationen
             dgCustomerInfo.ItemsSource = _db.Kunden.Where(x => x.AD_ADRESS_ID == selKunde.AD_ADRESS_ID)
                 .Select(x => new GridKunde
                 {
@@ -202,6 +222,7 @@ namespace SchauerMarkenerkennung.MVVM.View
                     AD_POSTLEITZAHL = x.AD_POSTLEITZAHL
                 }).ToList();
 
+            //Information wird Eingeblendet
             dgCustomerInfo.Visibility = Visibility.Visible;
         }
     }
