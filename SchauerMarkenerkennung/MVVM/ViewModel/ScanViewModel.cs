10e39429060fd6ef62d57dfa3d47a6a329d4afd2
ffischer18 <Fischer.Fabian@sus.htl-grieskirchen.at>
2022-06-14 17:54:25 +0200

    Added ClassDiagrams and Deleted unused Dto

--- SchauerMarkenerkennung/MVVM/ViewModel/ScanViewModel.cs
+++ SchauerMarkenerkennung/MVVM/ViewModel/ScanViewModel.cs
@@ -1,6 +1,4 @@
-﻿using MarkenLib;
-using SchauerMarkenerkennung.Core;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,59 +6,7 @@ using System.Threading.Tasks;
 
 namespace SchauerMarkenerkennung.MVVM.ViewModel
 {
-    public class ScanViewModel : ObservableObject
+    public class ScanViewModel
     {
-        bool isStarted = true;
-        //MarkenContext _db = new MarkenContext();
-
-        //List<Kunde> testList = new List<Kunde>();
-        //public ScanViewModel()
-        //{
-        //    testList = _db.Kunden.Select(x => x).ToList();
-        //}
-
-        private List<string> _markenNummern = new();
-
-        public List<string> MarkenNummern
-        {
-            get { return _markenNummern; }
-            set
-            {
-                _markenNummern = value;
-                OnPropertyChanged(nameof(MarkenNummern));
-            }
-        }
-
-        private string _markenNr = "";
-        public string MarkenNr
-        {
-            get { return _markenNr; }
-            set
-            { 
-                _markenNr = value;
-                if(value.Length > 8)
-                CheckForInput();
-                OnPropertyChanged(nameof(_markenNr));
-
-            }
-        }
-        
-        private async void CheckForInput()
-        {
-            if (isStarted && MarkenNr.Length > 8)
-            {
-                await AddToListBox();
-            }
-        }
-
-        private async Task<string> AddToListBox()
-        {
-            await Task.Delay(1000);
-            if (MarkenNr.Length > 8)
-                MarkenNummern.Add(MarkenNr);
-            MarkenNr = "";
-            return "";
-        }
-
     }
 }
